dbe5f5bf91451e71db61e15a403426c636298f7f
joannasys <joannas.y@live.com.sg>
2021-09-20 02:23:47 +0800

    save

--- codes/sql_pt_2.sql
+++ codes/sql_pt_2.sql
@@ -5,7 +5,8 @@ with tot_bill as (
 	max(id.bill_id) id_ -- unique identifier
 	,id.patient_id -- patient identifier
 	,round(sum(amount),2) tot_bill_yr -- total bill
-	,date_part('year',date_of_admission) year_
+	,date_part('year',date_of_admission) year_ -- admission year
+	,count(distinct date_of_admission) no_enc -- number of encounter per year
 
 	from bill_id id
 		left join bill_amount amt on id.bill_id = amt.bill_id
@@ -32,16 +33,29 @@ with tot_bill as (
 
 select id_
 ,bill.patient_id
-/*
+,bill.no_enc
 ,medical_history_1, medical_history_2_c,medical_history_3_c,medical_history_4,medical_history_5_c,medical_history_6,medical_history_7
 ,(medical_history_1+medical_history_2_c+medical_history_3_c+medical_history_4+medical_history_5_c+medical_history_6+medical_history_7) no_med_hist -- number of medical history
 ,cast(bill.year_ - DATE_PART('year', date_of_birth::date)as integer) age_at_yr -- age at year
+,case when cast(bill.year_ - DATE_PART('year', date_of_birth::date)as integer) < 56 then 0 else 1 end age_grp
 ,case when gender in ('Female','f') then 1 when gender in ('Male','m') then 0 else null end gender_c-- standardise female=1, male=0
 ,case when race in ('Indian','India') then 1 when race in ('Chinese','chinese') then 2 when race in ('Malay') then 3 when race in ('Others') then 4 else null end race_c -- standardise
 ,case when resident_status in ('Singaporean','Singapore citizen') then 0 when resident_status = 'PR' then 1 when resident_status = 'Foreigner' then 2 else null end resident_status_c -- standardise singporean=0 pr=1 foreigner=2
+,avg_bmi
+,case when avg_bmi > 27.5 then 4 when avg_bmi > 23 then 3 when avg_bmi > 18.5 then 2 else 1 end bmi_risk -- bmi risk from healthhub.sg 
 
-,tot_bill_yr
-*/
+,case when avg_bmi > 27.5 then 1 else 0 end bmi_high
+,case when race in ('Indian','India') then 1 else 0 end indian
+,case when race in ('Chinese','chinese') then 1 else 0 end chinese
+,case when race in ('Malay') then 1 else 0 end malay
+,case when race in ('Others') then 1 else 0 end other
+
+,case when resident_status in ('Singaporean','Singapore citizen') then 1 else 0 end sg
+,case when resident_status = 'PR' then 1 else 0 end pr
+,case when resident_status = 'Foreigner' then 1 else 0 end foreigner
+
+,tot_bill_yr 
+
 from tot_bill bill 
 
 	left join demographics demo on bill.patient_id = demo.patient_id
